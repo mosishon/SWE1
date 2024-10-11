5118722afd6b530084937447fa10c88c96899948
mosTafa Arshadi <mostafa1382ma@gmail.com>
2024-10-11 18:01:32 +0330

    [~] Fix Student database model

--- src/student/models.py
+++ src/student/models.py
@@ -8,6 +8,9 @@ from src.models import BaseModel
 # NOT FULL VERSION
 class Student(BaseModel):
     __tablename__ = "student"
-    student_id: Mapped[StudnentID] = mapped_column(index=True)
-
-    for_user: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
+    student_id: Mapped[StudnentID] = mapped_column(
+        primary_key=True, index=True, unique=True, nullable=False
+    )
+    for_user: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), index=True, nullable=False
+    )
