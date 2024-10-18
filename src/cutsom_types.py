f6aaff2a5a99a3e214ba84eae0ec2ae4bf62cf2c
mosTafa Arshadi <mostafa1382ma@gmail.com>
2024-10-18 18:59:28 +0330

    [~] Change phone number,national id,student id fro m int to str

--- src/cutsom_types.py
+++ src/cutsom_types.py
@@ -1,7 +1,7 @@
 from typing import NewType
 
 HashedPassword = NewType("HashedPassword", str)
-PhoneNumber = NewType("PhoneNumber", int)
-NationalID = NewType("NationalID", int)
-StudnentID = NewType("StudnentID", int)
+PhoneNumber = NewType("PhoneNumber", str)
+NationalID = NewType("NationalID", str)
+StudnentID = NewType("StudnentID", str)
 TimeStamp = NewType("TimeStamp", float)
