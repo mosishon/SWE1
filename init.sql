b033a156c855c6ca98ddaf4d0e45162b56e42458
mosTafa Arshadi <mostafa1382ma@gmail.com>
2024-11-23 02:48:35 +0330

    change create database query

--- init.sql
+++ init.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE IF NOT EXISTS swe;
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'swe'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE swe');
+   END IF;
+END
+$$;
